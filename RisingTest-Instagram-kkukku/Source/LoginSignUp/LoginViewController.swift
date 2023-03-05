7c354288281c31755e515766bf7b726ae266f85a
Iksang Yoo <andrew4477@naver.com>
2023-03-04 19:38:16 -0500

    Custom TextFied

--- RisingTest-Instagram-kkukku/Source/LoginSignUp/LoginViewController.swift
+++ RisingTest-Instagram-kkukku/Source/LoginSignUp/LoginViewController.swift
@@ -8,16 +8,35 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-    @IBOutlet weak var emailTF: UITextField!
-    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var emailTF: CustomTextField!
+    @IBOutlet weak var passwordTF: CustomTextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        textFieldLayout()
+        setTextField()
     }
 
-    func textFieldLayout() {
+    func setTextField() {
         emailTF.loginSignUpTextFieldUI(placeHolder: "사용자 이름, 이메일 주소 또는 휴대폰 번호")
         passwordTF.loginSignUpTextFieldUI(placeHolder: "비밀번호")
+        emailTF.addUILabel()
+        passwordTF.addUILabel()
+    }
+    
+    
+}
+
+extension LoginViewController: UITextFieldDelegate {
+
+    
+//    func textFieldDidBeginEditing(_ textField: UITextField) {
+//        print("begin")
+//
+//        textField.attributedPlaceholder = NSAttributedString(string: textField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.1289084852, green: 0.1131337956, blue: 0.1604926884, alpha: 0)])
+//    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+            
+        
     }
 }
--- RisingTest-Instagram-kkukku/Utilities/Extension/CustomTextField.swift
+++ RisingTest-Instagram-kkukku/Utilities/Extension/CustomTextField.swift
@@ -11,16 +11,9 @@ import UIKit
 class CustomTextField: UITextField {
     let label = UILabel()
     
-    override func textRect(forBounds bounds: CGRect) -> CGRect {
-            return bounds.insetBy(dx: 10, dy: 5) // 텍스트 영역을 좌우로 10pt, 위아래로 5pt 내립니다.
-        }
-
-        override func editingRect(forBounds bounds: CGRect) -> CGRect {
-            return bounds.insetBy(dx: 10, dy: 5) // 수정 모드일 때 텍스트 영역도 같은 크기로 사용합니다.
-        }
-    
-    func makePlaceHolderGoUp(_ isTextNil: Bool) {
+    func addUILabel() {
         self.addSubview(label)
+        self.delegate = self
         
         // Set label text
         label.text = self.placeholder
@@ -34,11 +27,63 @@ class CustomTextField: UITextField {
             label.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
             label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15)
         ])
+        label.isHidden = true
+    }
+    
+    func makeLabelHidden() {
+        label.isHidden = true
+    }
+    
+    func makeLabelShow() {
+        label.isHidden = false
+    }
+    
+    override func textRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 10, dy: 5) // 텍스트 영역을 좌우로 10pt, 위아래로 5pt 내립니다.
+        }
+
+        override func editingRect(forBounds bounds: CGRect) -> CGRect {
+            return bounds.insetBy(dx: 10, dy: 5) // 수정 모드일 때 텍스트 영역도 같은 크기로 사용합니다.
+        }
+    
+    func showPlaceHolderAgain() {
+        let font = UIFont(name: "Arial", size: 18)!
+        let fontDescriptor = font.fontDescriptor.withSymbolicTraits(.traitLooseLeading)!
+        let boldFont = UIFont(descriptor: fontDescriptor, size: 0)
+        let placeholderAttributes = [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.5416380763, green: 0.5816664696, blue: 0.6238328815, alpha: 1),
+                                     NSAttributedString.Key.font: boldFont]
         
-       
-            label.removeFromSuperview()
-        
-        
-        print("4. \(label.text)")
+        if self.text == "" {
+            makeLabelHidden()
+            self.attributedPlaceholder = NSAttributedString(string: self.placeholder!,
+                                                            attributes: placeholderAttributes)
+        }
+    }
+//    func makePlaceHolderGoUp(_ isTextNil: Bool) {
+//        self.addSubview(label)
+//
+//        // Set label text
+//        label.text = self.placeholder
+//        // Set label font and color
+//        label.font = UIFont.systemFont(ofSize: 13)
+//        label.textColor = UIColor.white
+//        // Enable Auto Layout
+//        label.translatesAutoresizingMaskIntoConstraints = false
+//        // Align label to the top left corner of text field
+//        NSLayoutConstraint.activate([
+//            label.topAnchor.constraint(equalTo: self.topAnchor, constant: 8),
+//            label.leadingAnchor.constraint(equalTo: self.leadingAnchor, constant: 15)
+//        ])
+//    }
+}
+
+extension CustomTextField: UITextFieldDelegate {
+    func textFieldDidBeginEditing(_ textField: UITextField) {
+        makeLabelShow()
+        textField.attributedPlaceholder = NSAttributedString(string: textField.placeholder!, attributes: [NSAttributedString.Key.foregroundColor: #colorLiteral(red: 0.1289084852, green: 0.1131337956, blue: 0.1604926884, alpha: 0)])
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        showPlaceHolderAgain()
     }
 }
